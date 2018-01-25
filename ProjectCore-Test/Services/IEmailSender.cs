02d1666aa2434deeae0c8695e850f3b3dd2cf97c
Tin <tintintiamzon@outlook.com>
2018-01-25 11:23:21 +0800

    initial commit

---
+++ ProjectCore-Test/Services/IEmailSender.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectCore_Test.Services
+{
+    public interface IEmailSender
+    {
+        Task SendEmailAsync(string email, string subject, string message);
+    }
+}
